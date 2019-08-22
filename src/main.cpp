34e3f391bd077e867ca4c79f49090c55464f25bd
Luis González <luisglez02@gmail.com>
2019-08-22 13:16:18 +0200

    Improved starting menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+#include <limits>
 #include "game/GameManager.h"
 #include "game/util/GameExceptions.h"
 
 int main() {
 
+    bool keepAskingForFile = true;
+    std::string filename;
+
+    std::cout << "Welcome to the COmmand-LIne-NArration engine!\n"
+                 "Made by Yxuer because he was bored.\n"
+                 "Please write the name of the game file you wish to load.\n"
+                 ">>> ";
+
+    do {
+        std::cin >> filename;
+        std::cout << "\n";
+        if (FILE *file = fopen(filename.c_str(), "r")) {
+            fclose(file);
+            keepAskingForFile = false;
+        } else {
+            std::cout << "That file does not exist. Please, write\n"
+                         "the name of an existing game file.\n"
+                         ">>> ";
+        }
+    } while(keepAskingForFile);
+
     GameManager *game = GameManager::getInstance();
 
     try {
-        game->loadXML("test.xml");
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        game->loadXML(filename);
         game->startGame();
     } catch (FatalException &e) {
         std::cout << e.what();
